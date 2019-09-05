efd22f278b4f333ff1575f35db41f84e12e013ad
Amandeep Singh <adsoapps@gmail.com>
2019-09-05 11:00:44 +0530

    Publishing correct Kerala Darshan project
    Correct vuforia configs

---
+++ Assets/Resources/Scripts/BtnTour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+
+public class BtnTour : MonoBehaviour, IVirtualButtonEventHandler {
+
+	public GameObject vb_Object;
+	public VideoPlayer tourismResult;
+
+	// Use this for initialization
+	void Start () {
+		vb_Object = GameObject.Find ("BtnTourism");
+		vb_Object.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+
+		tourismResult = GameObject.Find ("TourismResult").GetComponent<VideoPlayer> ();
+	}
+	
+	public void OnButtonPressed(VirtualButtonBehaviour vb){
+		tourismResult.Play ();
+		Debug.Log ("Pressed");
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb){
+		tourismResult.Pause ();
+		Debug.Log ("Released");
+	}
+}
